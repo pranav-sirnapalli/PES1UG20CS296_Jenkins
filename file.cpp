51030b7b4e82dfbf58f5656f73990575e17329ea
pranav sirnapalli <68234635+pranav-sirnapalli@users.noreply.github.com>
2023-02-17 15:08:08 +0530

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << " Jenkins Pipeline is a combination of plugins that support the integration and implementation of continuous delivery pipelines using Jenkins.\n";
+  cout << "Code has been modeified\n";
+  return 0;
+}
